88243a86d81ccf79ac95b671af65cbdcdd220e0e
Manuel Franco <manuel.developer.fm@gmail.com>
2023-11-20 18:56:08 +0000

    stg_promos-hace falta runear dbt

--- models/staging/sql_server_dbo/stg_promos.sql
+++ models/staging/sql_server_dbo/stg_promos.sql
@@ -1,6 +1,4 @@
-with 
-
-source as (
+with source as (
 
     select * from {{ source('sql_server_dbo', 'promos') }}
 
@@ -9,14 +7,15 @@ source as (
 renamed as (
 
     select
-        promo_id,
-        discount,
-        status,
-        _fivetran_deleted,
-        _fivetran_synced
-
+        cast(
+            {{dbt_utils.surrogate_key("promo_id")}}as varchar(50)
+        )as promo_id,
+        cast(promo_id as varchar(50))as des_promo
     from source
-
 )
 
 select * from renamed
+union all
+select
+    '9999' as promo_id,
+    'sin promo' as des_promo
